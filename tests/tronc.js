5ffaf2b5d2e57897b9c232dd575407dad1a5c6c6
charlex <charley@bodkin.me>
2017-06-30 16:31:06 -0700

    updated validation to 0.5.9

--- tests/tronc.js
+++ tests/tronc.js
@@ -8,23 +8,23 @@ var should = require('should'),
     ans = require('../lib/ans');
 
 var validate = function(json_path, done_callback) {
-    ans.getValidatorForVersion('0.5.8', function(err, validator) {
+    ans.getValidatorForVersion('0.5.9', function(err, validator) {
       var errors = validator.validate(require(json_path));
       if(errors) {
         console.warn('-------Validation errors--------');
         console.warn(errors);
         console.warn('--------------------------------');
       }
-      errors.length.should.eql(-1);
+      errors.length.should.eql(0);
       done_callback();
     });
 }
 
 describe("Tronc schemas", function() {
 
-  // it("Story schema should not have any errors", function(done_callback) {
-  //   validate('./fixtures/tronc/story.json', done_callback);
-  // });
+  it("Story schema should not have any errors", function(done_callback) {
+    validate('./fixtures/tronc/story.json', done_callback);
+  });
 
   it("Liveblog schema should not have any errors", function(done_callback) {
     validate('./fixtures/tronc/liveblog.json', done_callback);
